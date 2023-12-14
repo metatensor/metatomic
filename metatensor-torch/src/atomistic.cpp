Luthaf/metatensor

Record who requested a given neighbors list

metatensor-torch/src/atomistic.cpp
@@ -12,7 +12,50 @@
 using namespace metatensor_torch;
 
 
+NeighborsListOptionsHolder::NeighborsListOptionsHolder(
+    double cutoff,
+    bool full_list,
+    std::string requestor
+):
+    cutoff_(cutoff),
+    full_list_(full_list)
+{
+    this->add_requestor(std::move(requestor));
+}
+
+
+void NeighborsListOptionsHolder::add_requestor(std::string requestor) {
+    if (requestor.empty()) {
+        return;
+    }
+
+    for (const auto& existing: requestors_) {
+        if (requestor == existing) {
+            return;
+        }
+    }
+
+    requestors_.emplace_back(requestor);
+}
+
 std::string NeighborsListOptionsHolder::repr() const {
+    auto ss = std::ostringstream();
+
+    ss << "NeighborsListOptions\n";
+    ss << "    cutoff: " << std::to_string(cutoff_) << "\n";
+    ss << "    full_list: " << (full_list_ ? "True" : "False") << "\n";
+
+    if (!requestors_.empty()) {
+        ss << "    requested by:\n";
+        for (const auto& requestor: requestors_) {
+            ss << "        - " << requestor << "\n";
+        }
+    }
+
+    return ss.str();
+}
+
+std::string NeighborsListOptionsHolder::str() const {
     return "NeighborsListOptions(cutoff=" + std::to_string(cutoff_) + \
         ", full_list=" + (full_list_ ? "True" : "False") + ")";
 }
@@ -175,7 +218,7 @@ void SystemHolder::add_neighbors_list(NeighborsListOptions options, TorchTensorB
     auto it = neighbors_.find(options);
     if (it != neighbors_.end()) {
         C10_THROW_ERROR(ValueError,
-            "the neighbors list for " + options->repr() + " already exists in this system"
+            "the neighbors list for " + options->str() + " already exists in this system"
         );
     }
 
@@ -186,7 +229,7 @@ TorchTensorBlock SystemHolder::get_neighbors_list(NeighborsListOptions options) 
     auto it = neighbors_.find(options);
     if (it == neighbors_.end()) {
         C10_THROW_ERROR(ValueError,
-            "No neighbors list for " + options->repr() + " was found.\n"
+            "No neighbors list for " + options->str() + " was found.\n"
             "Is it part of the `requested_neighbors_lists` for this model?"
         );
     }

metatensor-torch/src/register.cpp
@@ -234,13 +234,17 @@ TORCH_LIBRARY(metatensor, m) {
     // ====================================================================== //
     m.class_<NeighborsListOptionsHolder>("NeighborsListOptions")
         .def(
-            torch::init<double, bool>(), DOCSTRING,
-            {torch::arg("cutoff"), torch::arg("full_list")}
+            torch::init<double, bool, std::string>(), DOCSTRING,
+            {torch::arg("cutoff"), torch::arg("full_list"), torch::arg("requestor") = ""}
         )
         .def_property("cutoff", &NeighborsListOptionsHolder::cutoff)
         .def_property("full_list", &NeighborsListOptionsHolder::full_list)
+        .def("requestors", &NeighborsListOptionsHolder::requestors)
+        .def("add_requestor", &NeighborsListOptionsHolder::add_requestor, DOCSTRING,
+            {torch::arg("requestor")}
+        )
         .def("__repr__", &NeighborsListOptionsHolder::repr)
-        .def("__str__", &NeighborsListOptionsHolder::repr)
+        .def("__str__", &NeighborsListOptionsHolder::str)
         .def("__eq__", static_cast<bool (*)(const NeighborsListOptions&, const NeighborsListOptions&)>(operator==))
         .def("__ne__", static_cast<bool (*)(const NeighborsListOptions&, const NeighborsListOptions&)>(operator!=))
         .def_pickle(

python/metatensor-torch/metatensor/torch/atomistic/documentation.py
@@ -108,13 +108,16 @@ class System:
 class NeighborsListOptions:
     """Options for the calculation of a neighbors list"""
 
-    def __init__(self, cutoff: float, full_list: bool):
-        ...
+    def __init__(self, cutoff: float, full_list: bool, requestor: str = ""):
+        """
+        :param cutoff: spherical cutoff radius for the neighbors list
+        :param full_list: should the list be a full or half neighbors list
+        :param requestor: who requested this neighbors list
+        """
 
     @property
     def cutoff(self) -> float:
         """Spherical cutoff radius for this neighbors list"""
-        ...
 
     @property
     def full_list(self) -> bool:
@@ -122,7 +125,14 @@ class NeighborsListOptions:
         Should the list be a full neighbors list (contains both the pair ``i->j`` and
         ``j->i``) or a half neighbors list (contains only the pair ``i->j``)
         """
-        ...
+
+    def requestors(self) -> List[str]:
+        """Get the list of modules requesting this neighbors list"""
+
+    def add_requestor(self, requestor: str):
+        """
+        Add another ``requestor`` to the list of modules requesting this neighbors list
+        """
 
     def __repr__(self) -> str:
         ...

python/metatensor-torch/tests/atomistic/neighbors.py
@@ -1,0 +1,114 @@
+from typing import Dict, List
+
+import torch
+
+from metatensor.torch import TensorBlock
+from metatensor.torch.atomistic import (
+    MetatensorAtomisticModule,
+    ModelCapabilities,
+    ModelRunOptions,
+    NeighborsListOptions,
+    System,
+)
+
+
+def test_neighbors_lists_options():
+    options = NeighborsListOptions(3.4, True, "hello")
+
+    assert options.cutoff == 3.4
+    assert options.full_list
+    assert options.requestors() == ["hello"]
+
+    options.add_requestor("another one")
+    assert options.requestors() == ["hello", "another one"]
+
+    # No empty requestors, no duplicated requestors
+    options.add_requestor("")
+    options.add_requestor("hello")
+    assert options.requestors() == ["hello", "another one"]
+
+    assert NeighborsListOptions(3.4, True, "a") == NeighborsListOptions(3.4, True, "b")
+    assert NeighborsListOptions(3.4, True) != NeighborsListOptions(3.4, False)
+    assert NeighborsListOptions(3.4, True) != NeighborsListOptions(3.5, True)
+
+    expected = "NeighborsListOptions(cutoff=3.400000, full_list=True)"
+    assert str(options) == expected
+
+    expected = """NeighborsListOptions
+    cutoff: 3.400000
+    full_list: True
+    requested by:
+        - hello
+        - another one
+"""
+    assert repr(options) == expected
+
+
+class ExampleModule(torch.nn.Module):
+    def __init__(self, name):
+        super().__init__()
+        self._name = name
+
+    def forward(
+        self, system: System, run_options: ModelRunOptions
+    ) -> Dict[str, TensorBlock]:
+        return {}
+
+    def requested_neighbors_lists(self) -> List[NeighborsListOptions]:
+        return [NeighborsListOptions(1.0, False, self._name)]
+
+
+class OtherModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(
+        self, system: System, run_options: ModelRunOptions
+    ) -> Dict[str, TensorBlock]:
+        return {}
+
+    def requested_neighbors_lists(self) -> List[NeighborsListOptions]:
+        return [NeighborsListOptions(2.0, True, "other module")]
+
+
+class FullModel(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.first = ExampleModule("first module")
+        self.second = ExampleModule("second module")
+        self.other = OtherModule()
+
+    def forward(
+        self, system: System, run_options: ModelRunOptions
+    ) -> Dict[str, TensorBlock]:
+        result = self.first(system, run_options)
+        result.update(self.second(system, run_options))
+        result.update(self.other(system, run_options))
+
+        return result
+
+
+def test_requested_neighbors_lists():
+    model = FullModel()
+    model.train(False)
+
+    atomistic = MetatensorAtomisticModule(model, ModelCapabilities())
+    requests = atomistic.requested_neighbors_lists()
+
+    assert len(requests) == 2
+
+    assert requests[0].cutoff == 1.0
+    assert not requests[0].full_list
+    assert requests[0].requestors() == [
+        "first module",
+        "FullModel.first",
+        "second module",
+        "FullModel.second",
+    ]
+
+    assert requests[1].cutoff == 2.0
+    assert requests[1].full_list
+    assert requests[1].requestors() == [
+        "other module",
+        "FullModel.other",
+    ]

metatensor/metatomic

metatensor-torch/src/atomistic.cpp
@@ -12,8 +12,52 @@
 using namespace metatensor_torch;
 
 
+NeighborsListOptionsHolder::NeighborsListOptionsHolder(
+    double model_cutoff,
+    bool full_list,
+    std::string requestor
+):
+    model_cutoff_(model_cutoff),
+    engine_cutoff_(model_cutoff),
+    full_list_(full_list)
+{
+    this->add_requestor(std::move(requestor));
+}
+
+
+void NeighborsListOptionsHolder::add_requestor(std::string requestor) {
+    if (requestor.empty()) {
+        return;
+    }
+
+    for (const auto& existing: requestors_) {
+        if (requestor == existing) {
+            return;
+        }
+    }
+
+    requestors_.emplace_back(requestor);
+}
+
 std::string NeighborsListOptionsHolder::repr() const {
-    return "NeighborsListOptions(model_cutoff=" + std::to_string(model_cutoff_) + \
+    auto ss = std::ostringstream();
+
+    ss << "NeighborsListOptions\n";
+    ss << "    model_cutoff: " << std::to_string(model_cutoff_) << "\n";
+    ss << "    full_list: " << (full_list_ ? "True" : "False") << "\n";
+
+    if (!requestors_.empty()) {
+        ss << "    requested by:\n";
+        for (const auto& requestor: requestors_) {
+            ss << "        - " << requestor << "\n";
+        }
+    }
+
+    return ss.str();
+}
+
+std::string NeighborsListOptionsHolder::str() const {
+    return "NeighborsListOptions(cutoff=" + std::to_string(model_cutoff_) + \
         ", full_list=" + (full_list_ ? "True" : "False") + ")";
 }
 
@@ -175,7 +219,7 @@ void SystemHolder::add_neighbors_list(NeighborsListOptions options, TorchTensorB
     auto it = neighbors_.find(options);
     if (it != neighbors_.end()) {
         C10_THROW_ERROR(ValueError,
-            "the neighbors list for " + options->repr() + " already exists in this system"
+            "the neighbors list for " + options->str() + " already exists in this system"
         );
     }
 
@@ -186,7 +230,7 @@ TorchTensorBlock SystemHolder::get_neighbors_list(NeighborsListOptions options) 
     auto it = neighbors_.find(options);
     if (it == neighbors_.end()) {
         C10_THROW_ERROR(ValueError,
-            "No neighbors list for " + options->repr() + " was found.\n"
+            "No neighbors list for " + options->str() + " was found.\n"
             "Is it part of the `requested_neighbors_lists` for this model?"
         );
     }

metatensor-torch/src/register.cpp
@@ -229,15 +229,19 @@ TORCH_LIBRARY(metatensor, m) {
     // ====================================================================== //
     m.class_<NeighborsListOptionsHolder>("NeighborsListOptions")
         .def(
-            torch::init<double, bool>(), DOCSTRING,
-            {torch::arg("model_cutoff"), torch::arg("full_list")}
+            torch::init<double, bool, std::string>(), DOCSTRING,
+            {torch::arg("model_cutoff"), torch::arg("full_list"), torch::arg("requestor") = ""}
         )
         .def_property("model_cutoff", &NeighborsListOptionsHolder::model_cutoff)
         .def_property("engine_cutoff", &NeighborsListOptionsHolder::engine_cutoff)
         .def("set_engine_unit", &NeighborsListOptionsHolder::set_engine_unit)
         .def_property("full_list", &NeighborsListOptionsHolder::full_list)
+        .def("requestors", &NeighborsListOptionsHolder::requestors)
+        .def("add_requestor", &NeighborsListOptionsHolder::add_requestor, DOCSTRING,
+            {torch::arg("requestor")}
+        )
         .def("__repr__", &NeighborsListOptionsHolder::repr)
-        .def("__str__", &NeighborsListOptionsHolder::repr)
+        .def("__str__", &NeighborsListOptionsHolder::str)
         .def("__eq__", static_cast<bool (*)(const NeighborsListOptions&, const NeighborsListOptions&)>(operator==))
         .def("__ne__", static_cast<bool (*)(const NeighborsListOptions&, const NeighborsListOptions&)>(operator!=))
         .def_pickle(

python/metatensor-torch/metatensor/torch/atomistic/documentation.py
@@ -111,8 +111,14 @@ class System:
 class NeighborsListOptions:
     """Options for the calculation of a neighbors list"""
 
-    def __init__(self, model_cutoff: float, full_list: bool):
-        ...
+    def __init__(self, model_cutoff: float, full_list: bool, requestor: str = ""):
+        """
+        :param model_cutoff: spherical cutoff radius for the neighbors list, in the
+            model units
+        :param full_list: should the list be a full or half neighbors list
+        :param requestor: who requested this neighbors list, you can add additional
+            requestors later using :py:meth:`add_requestor`
+        """
 
     @property
     def model_cutoff(self) -> float:
@@ -134,6 +140,14 @@ class NeighborsListOptions:
         """
         Should the list be a full neighbors list (contains both the pair ``i->j`` and
         ``j->i``) or a half neighbors list (contains only the pair ``i->j``)
+        """
+
+    def requestors(self) -> List[str]:
+        """Get the list of modules requesting this neighbors list"""
+
+    def add_requestor(self, requestor: str):
+        """
+        Add another ``requestor`` to the list of modules requesting this neighbors list
         """
 
     def __repr__(self) -> str:

python/metatensor-torch/tests/atomistic/neighbors.py
@@ -1,0 +1,116 @@
+from typing import Dict, List
+
+import torch
+from packaging import version
+
+from metatensor.torch import TensorBlock
+from metatensor.torch.atomistic import (
+    MetatensorAtomisticModule,
+    ModelCapabilities,
+    ModelRunOptions,
+    NeighborsListOptions,
+    System,
+)
+
+
+def test_neighbors_lists_options():
+    options = NeighborsListOptions(3.4, True, "hello")
+
+    assert options.model_cutoff == 3.4
+    assert options.full_list
+    assert options.requestors() == ["hello"]
+
+    options.add_requestor("another one")
+    assert options.requestors() == ["hello", "another one"]
+
+    # No empty requestors, no duplicated requestors
+    options.add_requestor("")
+    options.add_requestor("hello")
+    assert options.requestors() == ["hello", "another one"]
+
+    assert NeighborsListOptions(3.4, True, "a") == NeighborsListOptions(3.4, True, "b")
+    assert NeighborsListOptions(3.4, True) != NeighborsListOptions(3.4, False)
+    assert NeighborsListOptions(3.4, True) != NeighborsListOptions(3.5, True)
+
+    expected = "NeighborsListOptions(cutoff=3.400000, full_list=True)"
+    assert str(options) == expected
+
+    expected = """NeighborsListOptions
+    model_cutoff: 3.400000
+    full_list: True
+    requested by:
+        - hello
+        - another one
+"""
+    if version.parse(torch.__version__) >= version.parse("2.1"):
+        assert repr(options) == expected
+
+
+class ExampleModule(torch.nn.Module):
+    def __init__(self, name):
+        super().__init__()
+        self._name = name
+
+    def forward(
+        self, system: System, run_options: ModelRunOptions
+    ) -> Dict[str, TensorBlock]:
+        return {}
+
+    def requested_neighbors_lists(self) -> List[NeighborsListOptions]:
+        return [NeighborsListOptions(1.0, False, self._name)]
+
+
+class OtherModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(
+        self, system: System, run_options: ModelRunOptions
+    ) -> Dict[str, TensorBlock]:
+        return {}
+
+    def requested_neighbors_lists(self) -> List[NeighborsListOptions]:
+        return [NeighborsListOptions(2.0, True, "other module")]
+
+
+class FullModel(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.first = ExampleModule("first module")
+        self.second = ExampleModule("second module")
+        self.other = OtherModule()
+
+    def forward(
+        self, system: System, run_options: ModelRunOptions
+    ) -> Dict[str, TensorBlock]:
+        result = self.first(system, run_options)
+        result.update(self.second(system, run_options))
+        result.update(self.other(system, run_options))
+
+        return result
+
+
+def test_requested_neighbors_lists():
+    model = FullModel()
+    model.train(False)
+
+    atomistic = MetatensorAtomisticModule(model, ModelCapabilities())
+    requests = atomistic.requested_neighbors_lists()
+
+    assert len(requests) == 2
+
+    assert requests[0].model_cutoff == 1.0
+    assert not requests[0].full_list
+    assert requests[0].requestors() == [
+        "first module",
+        "FullModel.first",
+        "second module",
+        "FullModel.second",
+    ]
+
+    assert requests[1].model_cutoff == 2.0
+    assert requests[1].full_list
+    assert requests[1].requestors() == [
+        "other module",
+        "FullModel.other",
+    ]