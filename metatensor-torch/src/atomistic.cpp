Luthaf/metatensor

Add C++ classes for atomistic models input and capacities description

metatensor-torch/src/atomistic.cpp
@@ -1,0 +1,193 @@
+#include <cctype>
+#include <cstring>
+#include <sstream>
+
+#include <torch/torch.h>
+
+#include <metatensor.hpp>
+
+#include "metatensor/torch/atomistic.hpp"
+
+using namespace metatensor_torch;
+
+
+std::string NeighborsListOptionsHolder::repr() const {
+    return "NeighborsListOptions(cutoff=" + std::to_string(cutoff_) + \
+        ", full_list=" + (full_list_ ? "True" : "False") + ")";
+}
+
+// ========================================================================== //
+
+SystemHolder::SystemHolder(TorchTensorBlock positions_, TorchTensorBlock cell_):
+    positions(std::move(positions_)),
+    cell(std::move(cell_))
+{
+    // check the positions
+    auto samples_names = positions->samples()->names();
+    if (samples_names.size() != 2 || samples_names[0] != "atom" || samples_names[1] != "species") {
+        C10_THROW_ERROR(ValueError,
+            "invalid samples for `positions`: the samples names must be "
+            "'atom' and 'species'"
+        );
+    }
+
+    auto components = positions->components();
+    if (components.size() != 1 || *components[0] != metatensor::Labels({"xyz"}, {{0}, {1}, {2}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid components for `positions`: there should be a single 'xyz'=[0, 1, 2] component"
+        );
+    }
+
+    if (*positions->properties() != metatensor::Labels({"position"}, {{0}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid properties for `positions`: there should be a single 'positions'=0 property"
+        );
+    }
+
+    if (!positions->gradients_list().empty()) {
+        C10_THROW_ERROR(ValueError, "`positions` should not have any gradients");
+    }
+
+    // check the cell
+    if (*cell->samples() != metatensor::Labels({"_"}, {{0}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid samples for `cell`: there should be a single '_'=0 sample"
+        );
+    }
+
+    components = cell->components();
+    if (components.size() != 2) {
+        C10_THROW_ERROR(ValueError,
+            "invalid components for `cell`: there should be 2 components, got "
+            + std::to_string(components.size())
+        );
+    }
+
+    if (*components[0] != metatensor::Labels({"cell_abc"}, {{0}, {1}, {2}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid components for `cell`: the first component should be 'cell_abc'=[0, 1, 2]"
+        );
+    }
+
+    if (*components[1] != metatensor::Labels({"xyz"}, {{0}, {1}, {2}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid components for `cell`: the second component should be 'xyz'=[0, 1, 2]"
+        );
+    }
+
+    if (*cell->properties() != metatensor::Labels({"cell"}, {{0}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid properties for `cell`: there should be a single 'cell'=0 property"
+        );
+    }
+
+    if (!cell->gradients_list().empty()) {
+        C10_THROW_ERROR(ValueError, "`cell` should not have any gradients");
+    }
+}
+
+void SystemHolder::add_neighbors_list(NeighborsListOptions options, TorchTensorBlock neighbors) {
+    // check the structure of the NL
+    auto samples_names = neighbors->samples()->names();
+    if (samples_names.size() != 5 ||
+        samples_names[0] != "first_atom" ||
+        samples_names[1] != "second_atom" ||
+        samples_names[2] != "cell_shift_a" ||
+        samples_names[3] != "cell_shift_b" ||
+        samples_names[4] != "cell_shift_c"
+    ) {
+        C10_THROW_ERROR(ValueError,
+            "invalid samples for `neighbors`: the samples names must be "
+            "'first_atom', 'second_atom', 'cell_shift_a', 'cell_shift_b', 'cell_shift_c'"
+        );
+    }
+
+    // TODO: we could check that the values of first_atom and second_atom match
+    // entried in positions, but this might be a bit costly
+
+    auto components = neighbors->components();
+    if (components.size() != 1 || *components[0] != metatensor::Labels({"xyz"}, {{0}, {1}, {2}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid components for `neighbors`: there should be a single 'xyz'=[0, 1, 2] component"
+        );
+    }
+
+    if (*neighbors->properties() != metatensor::Labels({"distance"}, {{0}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid properties for `neighbors`: there should be a single 'distance'=0 property"
+        );
+    }
+
+    if (!neighbors->gradients_list().empty()) {
+        C10_THROW_ERROR(ValueError, "`neighbors` should not have any gradients");
+    }
+
+    // actually add the neighbors lists
+    auto it = neighbors_.find(options);
+    if (it != neighbors_.end()) {
+        C10_THROW_ERROR(ValueError,
+            "the neighbors list for " + options->repr() + " already exists in this system"
+        );
+    }
+
+    neighbors_.emplace(std::move(options), std::move(neighbors));
+}
+
+TorchTensorBlock SystemHolder::get_neighbors_list(NeighborsListOptions options) const {
+    auto it = neighbors_.find(options);
+    if (it == neighbors_.end()) {
+        C10_THROW_ERROR(ValueError,
+            "No neighbors list for " + options->repr() + " was found.\n"
+            "Is it part of the `requested_neighbors_lists` for this model?"
+        );
+    }
+    return it->second;
+}
+
+std::vector<NeighborsListOptions> SystemHolder::known_neighbors_lists() const {
+    auto result = std::vector<NeighborsListOptions>();
+    for (const auto& it: neighbors_) {
+        result.emplace_back(it.first);
+    }
+    return result;
+}
+
+
+void SystemHolder::add_data(std::string name, TorchTensorBlock values) {
+    if (name == "positions" || name == "cell" || name == "neighbors") {
+        C10_THROW_ERROR(ValueError, "custom data can not be 'positions', 'cell', or 'neighbors'");
+    }
+
+    if (data_.find(name) != data_.end()) {
+        C10_THROW_ERROR(ValueError, "custom data for '" + name + "' is already present in this system");
+    }
+
+    data_.emplace(std::move(name), std::move(values));
+}
+
+TorchTensorBlock SystemHolder::get_data(std::string name) const {
+    if (name == "positions") {
+        return positions;
+    } else if (name == "cell") {
+        return cell;
+    } else {
+        auto it = data_.find(name);
+        if (it == data_.end()) {
+            C10_THROW_ERROR(ValueError,
+                "no data for '" + name + "' found in this system"
+            );
+        }
+
+        TORCH_WARN_ONCE("custom data (", name,") is experimental, please contact the developers to add your data in the main API");
+        return it->second;
+    }
+}
+
+
+std::vector<std::string> SystemHolder::known_data() const {
+    auto result = std::vector<std::string>();
+    for (const auto& it: data_) {
+        result.emplace_back(it.first);
+    }
+    return result;
+}

metatensor-torch/src/register.cpp
@@ -1,3 +1,6 @@
+#include <iomanip>
+#include <sstream>
+
 #include <torch/script.h>
 
 #include "metatensor.hpp"
@@ -6,6 +9,7 @@
 #include "metatensor/torch/block.hpp"
 #include "metatensor/torch/tensor.hpp"
 #include "metatensor/torch/misc.hpp"
+#include "metatensor/torch/atomistic.hpp"
 
 using namespace metatensor_torch;
 
@@ -224,4 +228,89 @@ TORCH_LIBRARY(metatensor, m) {
 
     m.def("load", metatensor_torch::load);
     m.def("save", metatensor_torch::save);
+
+    // ====================================================================== //
+    //               code specific to atomistic simulations                   //
+    // ====================================================================== //
+    m.class_<NeighborsListOptionsHolder>("NeighborsListOptions")
+        .def(
+            torch::init<double, bool>(), DOCSTRING,
+            {torch::arg("cutoff"), torch::arg("full_list")}
+        )
+        .def_property("cutoff", &NeighborsListOptionsHolder::cutoff)
+        .def_property("full_list", &NeighborsListOptionsHolder::full_list)
+        .def("__repr__", &NeighborsListOptionsHolder::repr)
+        .def("__str__", &NeighborsListOptionsHolder::repr)
+        .def("__eq__", static_cast<bool (*)(const NeighborsListOptions&, const NeighborsListOptions&)>(operator==))
+        .def("__ne__", static_cast<bool (*)(const NeighborsListOptions&, const NeighborsListOptions&)>(operator!=))
+        ;
+
+
+    m.class_<SystemHolder>("System")
+        .def(
+            torch::init<TorchTensorBlock, TorchTensorBlock>(), DOCSTRING,
+            {torch::arg("positions"), torch::arg("cell")}
+        )
+        .def_readwrite("positions", &SystemHolder::positions)
+        .def_readwrite("cell", &SystemHolder::cell)
+        .def("add_neighbors_list", &SystemHolder::add_neighbors_list, DOCSTRING,
+            {torch::arg("options"), torch::arg("neighbors")}
+        )
+        .def("get_neighbors_list", &SystemHolder::get_neighbors_list, DOCSTRING,
+            {torch::arg("options")}
+        )
+        .def("known_neighbors_lists", &SystemHolder::known_neighbors_lists)
+        .def("add_data", &SystemHolder::add_data, DOCSTRING,
+            {torch::arg("name"), torch::arg("data")}
+        )
+        .def("get_data", &SystemHolder::get_data, DOCSTRING,
+            {torch::arg("name")}
+        )
+        .def("known_data", &SystemHolder::known_data)
+        ;
+
+
+    m.class_<ModelOutputHolder>("ModelOutput")
+        .def(
+            torch::init<std::string, std::string, bool, std::vector<std::string>>(),
+            DOCSTRING, {
+                torch::arg("quantity") = "",
+                torch::arg("unit") = "",
+                torch::arg("per_atom") = false,
+                torch::arg("forward_gradients") = std::vector<std::string>()
+            }
+        )
+        .def_readwrite("quantity", &ModelOutputHolder::quantity)
+        .def_readwrite("unit", &ModelOutputHolder::unit)
+        .def_readwrite("per_atom", &ModelOutputHolder::per_atom)
+        .def_readwrite("forward_gradients", &ModelOutputHolder::forward_gradients)
+        ;
+
+    m.class_<ModelCapabilitiesHolder>("ModelCapabilities")
+        .def(
+            torch::init<std::string, std::vector<int64_t>, torch::Dict<std::string, ModelOutput>>(),
+            DOCSTRING, {
+                torch::arg("length_unit") = "",
+                torch::arg("species") = std::vector<int64_t>(),
+                torch::arg("outputs") = torch::Dict<std::string, ModelOutput>(),
+            }
+        )
+        .def_readwrite("length_unit", &ModelCapabilitiesHolder::length_unit)
+        .def_readwrite("species", &ModelCapabilitiesHolder::species)
+        .def_readwrite("outputs", &ModelCapabilitiesHolder::outputs)
+        ;
+
+    m.class_<ModelRunOptionsHolder>("ModelRunOptions")
+        .def(
+            torch::init<std::string, torch::optional<std::vector<int64_t>>, torch::Dict<std::string, ModelOutput>>(),
+            DOCSTRING, {
+                torch::arg("length_unit") = "",
+                torch::arg("selected_atoms") = torch::nullopt,
+                torch::arg("outputs") = torch::Dict<std::string, ModelOutput>(),
+            }
+        )
+        .def_readwrite("length_unit", &ModelRunOptionsHolder::length_unit)
+        .def_readwrite("selected_atoms", &ModelRunOptionsHolder::selected_atoms)
+        .def_readwrite("outputs", &ModelRunOptionsHolder::outputs)
+        ;
 }

metatensor/metatomic

metatensor-torch/src/atomistic.cpp
@@ -1,0 +1,193 @@
+#include <cctype>
+#include <cstring>
+#include <sstream>
+
+#include <torch/torch.h>
+
+#include <metatensor.hpp>
+
+#include "metatensor/torch/atomistic.hpp"
+
+using namespace metatensor_torch;
+
+
+std::string NeighborsListOptionsHolder::repr() const {
+    return "NeighborsListOptions(model_cutoff=" + std::to_string(model_cutoff_) + \
+        ", full_list=" + (full_list_ ? "True" : "False") + ")";
+}
+
+// ========================================================================== //
+
+SystemHolder::SystemHolder(TorchTensorBlock positions_, TorchTensorBlock cell_):
+    positions(std::move(positions_)),
+    cell(std::move(cell_))
+{
+    // check the positions
+    auto samples_names = positions->samples()->names();
+    if (samples_names.size() != 2 || samples_names[0] != "atom" || samples_names[1] != "species") {
+        C10_THROW_ERROR(ValueError,
+            "invalid samples for `positions`: the samples names must be "
+            "'atom' and 'species'"
+        );
+    }
+
+    auto components = positions->components();
+    if (components.size() != 1 || *components[0] != metatensor::Labels({"xyz"}, {{0}, {1}, {2}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid components for `positions`: there should be a single 'xyz'=[0, 1, 2] component"
+        );
+    }
+
+    if (*positions->properties() != metatensor::Labels({"position"}, {{0}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid properties for `positions`: there should be a single 'positions'=0 property"
+        );
+    }
+
+    if (!positions->gradients_list().empty()) {
+        C10_THROW_ERROR(ValueError, "`positions` should not have any gradients");
+    }
+
+    // check the cell
+    if (*cell->samples() != metatensor::Labels({"_"}, {{0}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid samples for `cell`: there should be a single '_'=0 sample"
+        );
+    }
+
+    components = cell->components();
+    if (components.size() != 2) {
+        C10_THROW_ERROR(ValueError,
+            "invalid components for `cell`: there should be 2 components, got "
+            + std::to_string(components.size())
+        );
+    }
+
+    if (*components[0] != metatensor::Labels({"cell_abc"}, {{0}, {1}, {2}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid components for `cell`: the first component should be 'cell_abc'=[0, 1, 2]"
+        );
+    }
+
+    if (*components[1] != metatensor::Labels({"xyz"}, {{0}, {1}, {2}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid components for `cell`: the second component should be 'xyz'=[0, 1, 2]"
+        );
+    }
+
+    if (*cell->properties() != metatensor::Labels({"cell"}, {{0}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid properties for `cell`: there should be a single 'cell'=0 property"
+        );
+    }
+
+    if (!cell->gradients_list().empty()) {
+        C10_THROW_ERROR(ValueError, "`cell` should not have any gradients");
+    }
+}
+
+void SystemHolder::add_neighbors_list(NeighborsListOptions options, TorchTensorBlock neighbors) {
+    // check the structure of the NL
+    auto samples_names = neighbors->samples()->names();
+    if (samples_names.size() != 5 ||
+        samples_names[0] != "first_atom" ||
+        samples_names[1] != "second_atom" ||
+        samples_names[2] != "cell_shift_a" ||
+        samples_names[3] != "cell_shift_b" ||
+        samples_names[4] != "cell_shift_c"
+    ) {
+        C10_THROW_ERROR(ValueError,
+            "invalid samples for `neighbors`: the samples names must be "
+            "'first_atom', 'second_atom', 'cell_shift_a', 'cell_shift_b', 'cell_shift_c'"
+        );
+    }
+
+    // TODO: we could check that the values of first_atom and second_atom match
+    // entried in positions, but this might be a bit costly
+
+    auto components = neighbors->components();
+    if (components.size() != 1 || *components[0] != metatensor::Labels({"xyz"}, {{0}, {1}, {2}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid components for `neighbors`: there should be a single 'xyz'=[0, 1, 2] component"
+        );
+    }
+
+    if (*neighbors->properties() != metatensor::Labels({"distance"}, {{0}})) {
+        C10_THROW_ERROR(ValueError,
+            "invalid properties for `neighbors`: there should be a single 'distance'=0 property"
+        );
+    }
+
+    if (!neighbors->gradients_list().empty()) {
+        C10_THROW_ERROR(ValueError, "`neighbors` should not have any gradients");
+    }
+
+    // actually add the neighbors lists
+    auto it = neighbors_.find(options);
+    if (it != neighbors_.end()) {
+        C10_THROW_ERROR(ValueError,
+            "the neighbors list for " + options->repr() + " already exists in this system"
+        );
+    }
+
+    neighbors_.emplace(std::move(options), std::move(neighbors));
+}
+
+TorchTensorBlock SystemHolder::get_neighbors_list(NeighborsListOptions options) const {
+    auto it = neighbors_.find(options);
+    if (it == neighbors_.end()) {
+        C10_THROW_ERROR(ValueError,
+            "No neighbors list for " + options->repr() + " was found.\n"
+            "Is it part of the `requested_neighbors_lists` for this model?"
+        );
+    }
+    return it->second;
+}
+
+std::vector<NeighborsListOptions> SystemHolder::known_neighbors_lists() const {
+    auto result = std::vector<NeighborsListOptions>();
+    for (const auto& it: neighbors_) {
+        result.emplace_back(it.first);
+    }
+    return result;
+}
+
+
+void SystemHolder::add_data(std::string name, TorchTensorBlock values) {
+    if (name == "positions" || name == "cell" || name == "neighbors") {
+        C10_THROW_ERROR(ValueError, "custom data can not be 'positions', 'cell', or 'neighbors'");
+    }
+
+    if (data_.find(name) != data_.end()) {
+        C10_THROW_ERROR(ValueError, "custom data for '" + name + "' is already present in this system");
+    }
+
+    data_.emplace(std::move(name), std::move(values));
+}
+
+TorchTensorBlock SystemHolder::get_data(std::string name) const {
+    if (name == "positions") {
+        return positions;
+    } else if (name == "cell") {
+        return cell;
+    } else {
+        auto it = data_.find(name);
+        if (it == data_.end()) {
+            C10_THROW_ERROR(ValueError,
+                "no data for '" + name + "' found in this system"
+            );
+        }
+
+        TORCH_WARN_ONCE("custom data (", name,") is experimental, please contact the developers to add your data in the main API");
+        return it->second;
+    }
+}
+
+
+std::vector<std::string> SystemHolder::known_data() const {
+    auto result = std::vector<std::string>();
+    for (const auto& it: data_) {
+        result.emplace_back(it.first);
+    }
+    return result;
+}

metatensor-torch/src/register.cpp
@@ -1,9 +1,13 @@
+#include <iomanip>
+#include <sstream>
+
 #include <torch/script.h>
 
 #include "metatensor/torch/labels.hpp"
 #include "metatensor/torch/block.hpp"
 #include "metatensor/torch/tensor.hpp"
 #include "metatensor/torch/misc.hpp"
+#include "metatensor/torch/atomistic.hpp"
 
 using namespace metatensor_torch;
 
@@ -219,4 +223,92 @@ TORCH_LIBRARY(metatensor, m) {
 
     m.def("load", metatensor_torch::load);
     m.def("save", metatensor_torch::save);
+
+    // ====================================================================== //
+    //               code specific to atomistic simulations                   //
+    // ====================================================================== //
+    m.class_<NeighborsListOptionsHolder>("NeighborsListOptions")
+        .def(
+            torch::init<double, bool>(), DOCSTRING,
+            {torch::arg("model_cutoff"), torch::arg("full_list")}
+        )
+        .def_property("model_cutoff", &NeighborsListOptionsHolder::model_cutoff)
+        .def_property("engine_cutoff", &NeighborsListOptionsHolder::engine_cutoff)
+        .def("set_engine_unit", &NeighborsListOptionsHolder::set_engine_unit)
+        .def_property("full_list", &NeighborsListOptionsHolder::full_list)
+        .def("__repr__", &NeighborsListOptionsHolder::repr)
+        .def("__str__", &NeighborsListOptionsHolder::repr)
+        .def("__eq__", static_cast<bool (*)(const NeighborsListOptions&, const NeighborsListOptions&)>(operator==))
+        .def("__ne__", static_cast<bool (*)(const NeighborsListOptions&, const NeighborsListOptions&)>(operator!=))
+        ;
+
+
+    m.class_<SystemHolder>("System")
+        .def(
+            torch::init<TorchTensorBlock, TorchTensorBlock>(), DOCSTRING,
+            {torch::arg("positions"), torch::arg("cell")}
+        )
+        .def_readwrite("positions", &SystemHolder::positions)
+        .def_readwrite("cell", &SystemHolder::cell)
+        .def("__len__", &SystemHolder::size)
+        .def("add_neighbors_list", &SystemHolder::add_neighbors_list, DOCSTRING,
+            {torch::arg("options"), torch::arg("neighbors")}
+        )
+        .def("get_neighbors_list", &SystemHolder::get_neighbors_list, DOCSTRING,
+            {torch::arg("options")}
+        )
+        .def("known_neighbors_lists", &SystemHolder::known_neighbors_lists)
+        .def("add_data", &SystemHolder::add_data, DOCSTRING,
+            {torch::arg("name"), torch::arg("data")}
+        )
+        .def("get_data", &SystemHolder::get_data, DOCSTRING,
+            {torch::arg("name")}
+        )
+        .def("known_data", &SystemHolder::known_data)
+        ;
+
+
+    m.class_<ModelOutputHolder>("ModelOutput")
+        .def(
+            torch::init<std::string, std::string, bool, std::vector<std::string>>(),
+            DOCSTRING, {
+                torch::arg("quantity") = "",
+                torch::arg("unit") = "",
+                torch::arg("per_atom") = false,
+                torch::arg("forward_gradients") = std::vector<std::string>()
+            }
+        )
+        .def_readwrite("quantity", &ModelOutputHolder::quantity)
+        .def_readwrite("unit", &ModelOutputHolder::unit)
+        .def_readwrite("per_atom", &ModelOutputHolder::per_atom)
+        .def_readwrite("forward_gradients", &ModelOutputHolder::forward_gradients)
+        ;
+
+    m.class_<ModelCapabilitiesHolder>("ModelCapabilities")
+        .def(
+            torch::init<std::string, std::vector<int64_t>, torch::Dict<std::string, ModelOutput>>(),
+            DOCSTRING, {
+                torch::arg("length_unit") = "",
+                torch::arg("species") = std::vector<int64_t>(),
+                torch::arg("outputs") = torch::Dict<std::string, ModelOutput>(),
+            }
+        )
+        .def_readwrite("length_unit", &ModelCapabilitiesHolder::length_unit)
+        .def_readwrite("species", &ModelCapabilitiesHolder::species)
+        .def_readwrite("outputs", &ModelCapabilitiesHolder::outputs)
+        ;
+
+    m.class_<ModelRunOptionsHolder>("ModelRunOptions")
+        .def(
+            torch::init<std::string, torch::optional<std::vector<int64_t>>, torch::Dict<std::string, ModelOutput>>(),
+            DOCSTRING, {
+                torch::arg("length_unit") = "",
+                torch::arg("selected_atoms") = torch::nullopt,
+                torch::arg("outputs") = torch::Dict<std::string, ModelOutput>(),
+            }
+        )
+        .def_readwrite("length_unit", &ModelRunOptionsHolder::length_unit)
+        .def_readwrite("selected_atoms", &ModelRunOptionsHolder::selected_atoms)
+        .def_readwrite("outputs", &ModelRunOptionsHolder::outputs)
+        ;
 }