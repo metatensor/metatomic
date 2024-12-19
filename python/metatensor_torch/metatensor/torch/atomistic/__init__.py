metatensor/metatensor

Implement serialization for `System`

python/metatensor-torch/metatensor/torch/atomistic/__init__.py
@@ -34,6 +34,7 @@ else:
     register_autograd_neighbors = torch.ops.metatensor.register_autograd_neighbors
     unit_conversion_factor = torch.ops.metatensor.unit_conversion_factor
 
+from .io import load, save  # noqa: F401
 from .model import (  # noqa: F401
     MetatensorAtomisticModel,
     ModelInterface,

metatensor/metatomic

Co-Authored-By: Guillaume Fraux <guillaume.fraux@epfl.ch>

python/metatensor_torch/metatensor/torch/atomistic/__init__.py
@@ -34,6 +34,7 @@ else:
     register_autograd_neighbors = torch.ops.metatensor.register_autograd_neighbors
     unit_conversion_factor = torch.ops.metatensor.unit_conversion_factor
 
+from .io import load_system, save  # noqa: F401
 from .model import (  # noqa: F401
     MetatensorAtomisticModel,
     ModelInterface,