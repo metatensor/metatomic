c4edb14e68c25647b44ea1e999645c1671ff0e7e
Guillaume Fraux <guillaume.fraux@epfl.ch>
2025-09-15 09:37:00 +0200

    Clarify the use of custom build backend (#87)

--- python/metatomic_torch/build-backend/backend.py
+++ python/metatomic_torch/build-backend/backend.py
@@ -27,7 +27,4 @@ build_sdist = build_meta.build_sdist
 # Special dependencies to build the wheels
 def get_requires_for_build_wheel(config_settings=None):
     defaults = build_meta.get_requires_for_build_wheel(config_settings)
-    return defaults + [
-        TORCH_DEP,
-        "metatensor-torch >=0.8.0,<0.9.0",
-    ]
+    return defaults + [TORCH_DEP]
