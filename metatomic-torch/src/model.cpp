b16548cf59c013ec08311f56c8229c0ee9e46996
Filippo Bigi <98903385+frostedoyster@users.noreply.github.com>
2026-01-07 03:21:09 +0100

    Add SI units

--- metatomic-torch/src/model.cpp
+++ metatomic-torch/src/model.cpp
@@ -1204,6 +1204,7 @@ static std::map<std::string, Quantity> KNOWN_QUANTITIES = {
         {"u*A/fs", 1.0},
         {"u*A/ps", 1000.0},
         {"(eV*u)^(1/2)", 0.09822694743391452},
+        {"kg*m/s", 1.6605390666e-22},
     }, {
         // alternative names
     }}},
