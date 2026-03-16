eb0b5ccecc9ce1e63d09a6461fc5d96fac847377
GardevoirX <xqj.2001@outlook.com>
2026-03-16 13:37:23 +0100

    Fix CI

--- python/metatomic_torch/metatomic/torch/heat_flux.py
+++ python/metatomic_torch/metatomic/torch/heat_flux.py
@@ -230,7 +230,7 @@ class HeatFlux(torch.nn.Module):
         assert isinstance(model, AtomisticModel)
         self._model = model.module
         self._interaction_range = model.capabilities().interaction_range
-        if model.capabilities().length_unit not in ["angstrom", "A"]:
+        if model.capabilities().length_unit.lower() not in ["angstrom", "a"]:
             raise NotImplementedError(
                 f"HeatFluxWrapper only supports models with length unit 'angstrom' or "
                 f"'A', but got {model.capabilities().length_unit}"
