3ad4473f4ec7ef94065e6fc8b6c6daf363b7e7b3
Guillaume Fraux <guillaume.fraux@epfl.ch>
2026-04-22 17:49:02 +0200

    Add a helper for tests expected to print to stderr

--- python/metatomic_ase/tests/_tests_utils.py
+++ python/metatomic_ase/tests/_tests_utils.py
@@ -1,5 +1,7 @@
 import os
+import re
 
+import pytest
 import torch
 
 
@@ -26,3 +28,37 @@ STR_TO_DTYPE = {
     "float32": torch.float32,
     "float64": torch.float64,
 }
+
+
+class prints_to_stderr:
+    def __init__(self, capfd, match=""):
+        self.capfd = capfd
+        self.match = match
+
+    def __enter__(self):
+        out, err = self.capfd.readouterr()
+        if out != "":
+            pytest.fail(
+                "Expected no output to stdout before prints_to_stderr block, "
+                f"got:\n'{out}'"
+            )
+
+        if err != "":
+            pytest.fail(
+                "Expected no output to stderr before prints_to_stderr block, "
+                f"got:\n'{err}'"
+            )
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        out, err = self.capfd.readouterr()
+
+        if out != "":
+            pytest.fail(
+                f"Expected no output to stdout in prints_to_stderr block, got:\n'{out}'"
+            )
+
+        if re.search(self.match, err) is None:
+            pytest.fail(
+                "Expected output to stderr matching "
+                f"'{self.match}' in prints_to_stderr block, got:\n'{err}'"
+            )
