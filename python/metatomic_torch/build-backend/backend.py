63b9118ab8e36cf8595e25bebbb8e9d189bf0cd7
Guillaume Fraux <guillaume.fraux@epfl.ch>
2026-05-13 14:45:08 +0200

    Scaffold a new metatomic-core package

--- python/metatomic_torch/build-backend/backend.py
+++ python/metatomic_torch/build-backend/backend.py
@@ -1,11 +1,24 @@
 # This is a custom Python build backend wrapping setuptool's to only depend on
 # torch/metatensor-torch when building the wheel and not the sdist
 import os
+import pathlib
 
 from setuptools import build_meta
 
 
-ROOT = os.path.realpath(os.path.dirname(__file__))
+ROOT = pathlib.Path(__file__).parent.resolve()
+
+METATOMIC_CORE = (ROOT / ".." / ".." / "metatomic_core").resolve()
+METATOMIC_NO_LOCAL_DEPS = os.environ.get("METATOMIC_NO_LOCAL_DEPS", "0") == "1"
+
+
+if not METATOMIC_NO_LOCAL_DEPS and METATOMIC_CORE.exists():
+    # we are building from a git checkout
+    METATOMIC_CORE_DEP = f"metatomic-core @ {METATOMIC_CORE.as_uri()}"
+else:
+    # we are building from a sdist
+    METATOMIC_CORE_DEP = "metatomic-core >=0.1.0,<0.2"
+
 
 FORCED_TORCH_VERSION = os.environ.get("METATOMIC_TORCH_BUILD_WITH_TORCH_VERSION")
 if FORCED_TORCH_VERSION is not None:
@@ -27,7 +40,7 @@ build_sdist = build_meta.build_sdist
 # Special dependencies to build the wheels
 def get_requires_for_build_wheel(config_settings=None):
     defaults = build_meta.get_requires_for_build_wheel(config_settings)
-    return defaults + [TORCH_DEP]
+    return defaults + [TORCH_DEP, METATOMIC_CORE_DEP]
 
 
 def build_editable(wheel_directory, config_settings=None, metadata_directory=None):
