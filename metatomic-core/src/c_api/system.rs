103636e3ca1e8b66978e3bc081985a133da574c2
Guillaume Fraux <guillaume.fraux@epfl.ch>
2026-05-27 16:25:27 +0200

    Draft the C API for metatomic-core

---
+++ metatomic-core/src/c_api/system.rs
@@ -0,0 +1,157 @@
+use std::ffi::c_char;
+
+use dlpk::sys::DLManagedTensorVersioned;
+use metatensor::c_api::{mts_block_t, mts_tensormap_t};
+
+use crate::System;
+use super::{mta_status_t, mta_string_t};
+
+/// TODO
+#[allow(non_camel_case_types)]
+pub struct mta_system_t(pub(crate) System);
+
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_system_create(
+    length_unit: *const c_char,
+    types: *mut DLManagedTensorVersioned,
+    positions: *mut DLManagedTensorVersioned,
+    cell: *mut DLManagedTensorVersioned,
+    pbc: *mut DLManagedTensorVersioned,
+    system: *mut *mut mta_system_t,
+) -> mta_status_t {
+    todo!()
+}
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_system_free(system: *mut mta_system_t) -> mta_status_t {
+    todo!()
+}
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_system_size(
+    system: *const mta_system_t,
+    size: *mut usize,
+) -> mta_status_t {
+    todo!()
+}
+
+/// TODO
+#[allow(non_camel_case_types)]
+#[repr(C)]
+#[non_exhaustive]
+pub enum mta_system_data_kind {
+    MTA_SYSTEM_DATA_TYPES = 0,
+    MTA_SYSTEM_DATA_POSITIONS = 1,
+    MTA_SYSTEM_DATA_CELL = 2,
+    MTA_SYSTEM_DATA_PBC = 3,
+}
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_system_get_data(
+    system: *const mta_system_t,
+    request: mta_system_data_kind,
+    data: *mut *mut DLManagedTensorVersioned,
+) -> mta_status_t {
+    todo!()
+}
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_system_get_length_unit(
+    system: *const mta_system_t,
+    length_unit: *mut mta_string_t,
+) -> mta_status_t {
+    todo!()
+}
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_system_set_pairs(
+    system: *mut mta_system_t,
+    options: *const c_char,
+    pairs: *mut mts_block_t,
+) -> mta_status_t {
+    todo!()
+}
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_system_get_pairs(
+    system: *const mta_system_t,
+    options: *const c_char,
+    pairs: *mut *const mts_block_t,
+) -> mta_status_t {
+    todo!()
+}
+
+// TODO: should these be separate functions or a single
+// mta_system_pairs_options(system, &options, &count)?
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_system_pairs_count(
+    system: *const mta_system_t,
+    count: *mut usize,
+) -> mta_status_t {
+    todo!()
+}
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_system_pairs_options(
+    system: *const mta_system_t,
+    index: usize,
+    options: *mut mta_string_t,
+) -> mta_status_t {
+    todo!()
+}
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_system_set_custom_data(
+    system: *mut mta_system_t,
+    name: *const c_char,
+    data: *mut mts_tensormap_t,
+) -> mta_status_t {
+    todo!()
+}
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_system_get_custom_data(
+    system: *const mta_system_t,
+    name: *const c_char,
+    data: *mut *const mts_tensormap_t,
+) -> mta_status_t {
+    todo!()
+}
+
+// TODO: should these be separate functions or a single
+// mta_system_data_names(system, &names, &count)?
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_system_data_count(
+    system: *const mta_system_t,
+    count: *mut usize,
+) -> mta_status_t {
+    todo!()
+}
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_system_data_name(
+    system: *const mta_system_t,
+    index: usize,
+    name: *mut mta_string_t,
+) -> mta_status_t {
+    todo!()
+}
+
+
+// TODO: mta_system_to(device, dtype)
---
+++ metatomic-core/src/c_api/utils.rs
@@ -0,0 +1,114 @@
+use std::ffi::{CString, c_char};
+
+use once_cell::sync::Lazy;
+
+use super::mta_status_t;
+
+
+static VERSION: Lazy<CString> = Lazy::new(|| {
+    CString::new(env!("METATOMIC_FULL_VERSION")).expect("version contains NULL byte")
+});
+
+
+/// Get the runtime version of the metatomic library as a string.
+///
+/// This version follows the `<major>.<minor>.<patch>[-<dev>]` format.
+#[no_mangle]
+pub extern "C" fn mta_version() -> *const c_char {
+    return VERSION.as_ptr();
+}
+
+/// TODO
+#[allow(non_camel_case_types)]
+pub struct mta_opaque_string_t(CString);
+
+/// TODO
+#[allow(non_camel_case_types)]
+#[repr(transparent)]
+pub struct mta_string_t(*mut mta_opaque_string_t);
+
+impl std::fmt::Debug for mta_string_t {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut builder = f.debug_tuple("mta_string_t");
+
+        if self.0.is_null() {
+            builder.field(&"NULL");
+        } else {
+            builder.field(&self.as_str());
+        }
+        builder.finish()
+    }
+}
+
+impl mta_string_t {
+    /// TODO
+    pub fn new(value: impl Into<String>) -> Self {
+        let cstring = CString::new(value.into()).unwrap();
+        let boxed = Box::new(mta_opaque_string_t(cstring));
+        mta_string_t(Box::into_raw(boxed))
+    }
+
+    /// TODO
+    pub fn null() -> Self {
+        mta_string_t(std::ptr::null_mut())
+    }
+
+    /// TODO
+    pub fn as_str(&self) -> &str {
+        if self.0.is_null() {
+            return "";
+        }
+        unsafe {
+            return (*(self.0)).0.to_str().expect("mta_string_t is not valid UTF8")
+        }
+    }
+}
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_string_create(
+    raw: *const c_char,
+) -> mta_string_t {
+    todo!()
+}
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_string_free(string: mta_string_t) {
+    todo!()
+}
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_string_view(
+    string: mta_string_t,
+) -> *const c_char {
+    todo!()
+}
+
+
+/// TODO
+#[allow(non_camel_case_types)]
+#[repr(C)]
+#[derive(Debug)]
+pub struct mta_kv_pair_t {
+    /// TODO
+    pub key: *const c_char,
+    /// TODO
+    pub value: *const c_char,
+}
+
+
+/// TODO
+#[no_mangle]
+pub unsafe extern "C" fn mta_unit_conversion_factor(
+    from_unit: *const c_char,
+    to_unit: *const c_char,
+    conversion: *mut f64,
+) -> mta_status_t {
+    todo!()
+}
+
+
+
+// TODO: logging & warnings?
