1753a2bf65991f21b213bd28cdefa173ac3fd910
Johannes Spies <13813209+johannes-spies@users.noreply.github.com>
2026-05-28 16:18:18 +0200

    Implement mta_string_t in the C API

--- metatomic-core/tests/misc.cpp
+++ metatomic-core/tests/misc.cpp
@@ -1,3 +1,5 @@
+#include <cstring>
+
 #include <catch.hpp>
 
 #include "metatomic.h"
@@ -12,4 +14,36 @@ TEST_CASE("Version macros") {
 
     // METATOMIC_VERSION should start with `x.y.z`
     CHECK(std::string(METATOMIC_VERSION).find(version) == 0);
+}
+
+TEST_CASE("mta_string_t") {
+    auto* str = mta_string_create("hello");
+    REQUIRE(str != nullptr);
+
+    const char* view = mta_string_view(str);
+    CHECK(std::strlen(view) == 5);
+    CHECK(std::string(view) == "hello");
+    mta_string_free(str);
+
+    // empty string
+    str = mta_string_create("");
+    REQUIRE(str != nullptr);
+    CHECK(std::string(mta_string_view(str)) == "");
+    mta_string_free(str);
+
+    // special characters
+    str = mta_string_create("a\nb\tc\xFFºµ");
+    REQUIRE(str != nullptr);
+    CHECK(std::string(mta_string_view(str)) == std::string("a\nb\tc\xFFºµ"));
+    mta_string_free(str);
+
+    // long string
+    std::string long_str(10000, 'x');
+    str = mta_string_create(long_str.c_str());
+    REQUIRE(str != nullptr);
+    CHECK(std::string(mta_string_view(str)) == long_str);
+    mta_string_free(str);
+
+    // free on a null pointer should work
+    mta_string_free(nullptr);
 }
