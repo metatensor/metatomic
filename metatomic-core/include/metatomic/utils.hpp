a691f45e63c87add6fd32f2040155c2eed7a8e26
Rocco Meli <rocco.meli@cscs.ch>
2026-06-02 10:56:37 +0200

    Add unit conversion and error handling to C++

--- metatomic-core/include/metatomic/utils.hpp
+++ metatomic-core/include/metatomic/utils.hpp
@@ -1,7 +1,22 @@
 #pragma once
 
+#include <string>
+
 #include <metatomic.h>
+#include <metatomic/errors.hpp>
 
 namespace metatomic {
+
+    inline double unit_conversion_factor(
+        const std::string& from_unit,
+        const std::string& to_unit
+    ) {
+        double conversion = 0.0;
+
+        auto status = mta_unit_conversion_factor(from_unit.c_str(), to_unit.c_str(), &conversion);
+        details::check_status(status);
+
+        return conversion;
+    }
 
 } // namespace metatomic
